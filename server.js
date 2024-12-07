0f6d61869717055e118c8bf987bb4f4af4bd950a
Reigo Jool <ReigoJool7@gmail.com>
2024-12-07 20:16:05 +0200

    search bar

---
+++ server.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const axios = require('axios');
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/search', (req, res) => {
+    res.render('search', {movieData:''});
+});
+
+app.post('/search', (req, res) => {
+    let userMovieTitle = req.body.movieTitle;
+
+    let movieUrl = `https://api.themoviedb.org/3/search/movie?api_key=17b7cacc477fdc5b0bb8243ce0d96d0b&query=${userMovieTitle}`;
+    let genres = 'https://api.themoviedb.org/3/genre/movie/list?api_key=17b7cacc477fdc5b0bb8243ce0d96d0b&language=en-US';
+   
+    let endpoints = [movieUrl, genres];
+
+    axios.all(endpoints.map((endpoint) => axios.get(endpoint)))
+    .then(
+        axios.spread((movie, genres) => {
+            const [general, generalComingSoon] = movie.data.results;
+            const movieGenreIds = general.genre_ids;        
+            const movieGenres = genres.data.genres; 
+            
+            let genresArray = [];
+            for(let i = 0; i < movieGenreIds.length; i++) {
+                for(let j = 0; j < movieGenres.length; j++) {
+                    if(movieGenreIds[i] === movieGenres[j].id) {
+                        genresArray.push(movieGenres[j].name);
+                    }
+                    
+                }
+            }
+
+            let genresToDisplay = '';
+            genresArray.forEach(genre => {
+                genresToDisplay = genresToDisplay+ `${genre}, `;
+            });
+
+            genresToDisplay = genresToDisplay.slice(0, -2) + '.';
+            
+            let movieDetails = {
+                title: general.original_title,
+                year: new Date(general.release_date).getFullYear(),
+                overview: general.overview,
+                posterUrl: `https://image.tmdb.org/t/p/w500/${general.poster_path}`,
+                genres: genresToDisplay
+
+            };
+
+            res.render('search', {movieData: movieDetails});
+        })
+      );
+    
+});
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log('Server is running');
+});
